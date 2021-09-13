954264b9e0bbd7a0f7c09ec9bc8ecf0af5630458
XUWENJIE <xwjslivermoon@gmail.com>
2021-09-13 19:41:39 +0900

    marge test

--- Assets/marge.cs
+++ Assets/marge.cs
@@ -6,9 +6,10 @@ public class marge : MonoBehaviour
 {
     // Start is called before the first frame update
     int n;
+
     void Start()
     {
-        
+        n = n - 3;
     }
 
     // Update is called once per frame
