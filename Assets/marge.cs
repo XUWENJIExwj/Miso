3b10a3af9c682475cf74d216d6dbd683c048fe95
DESKTOP-RH7G0BO\yoh1019 <yohsuke1910@icloud.com>
2021-09-13 19:08:08 +0900

    マージスクリプトテスト

---
+++ Assets/marge.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class marge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int n;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
