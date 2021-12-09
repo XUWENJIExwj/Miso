7f3b75b4776e47443470e2c4de110d4223d89bba
DESKTOP-RH7G0BO\yoh1019 <yohsuke1910@icloud.com>
2021-12-09 11:44:48 +0900

    BGM・SEの追加<soundフォルダ

---
+++ Assets/Sound/SoundManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioClip[] clips;
+    AudioSource audios;
+
+    void Start()
+    {
+        audios = GetComponent<AudioSource>();
+    }
+
+   //以下各種SE
+
+   //ルート選択時
+    public void SE_Route()
+    {
+        audios.PlayOneShot(clips[0]);
+    }
+   //発進ボタン
+    public void SE_Go()
+    { 
+        audios.PlayOneShot(clips[1]);
+    }
+   //浄化時
+   public void SE_Clean()
+    {
+        audios.PlayOneShot(clips[2]);
+    }
+   //メインイベント発生時
+   public void SE_MainEvent()
+    {
+        audios.PlayOneShot(clips[3]);
+    }
+    //サブイベント発生時
+    public void SE_SubEvent()
+    {
+        audios.PlayOneShot(clips[4]);
+    }
+    //会話時
+    public void SE_Talk()
+    {
+        audios.PlayOneShot(clips[5]);
+    }
+    //リザルト画面の表示時
+    public void SE_Result1()
+    {
+        audios.PlayOneShot(clips[6]);
+    }
+    //リザルト画面でのイベント各種の表示時
+    public void SE_Result2()
+    {
+        audios.PlayOneShot(clips[7]);
+    }
+
+    //他タップ音
+    public void SE_Tap()
+    {
+        audios.PlayOneShot(clips[8]);
+    }
+}
